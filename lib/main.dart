660811438891a190342a527c1f67095e3b5260ec
zenasgram <zenasgram@gmail.com>
2020-05-23 21:53:08 +0800

    android internal test release

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class Spark extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
 //        textTheme: TextTheme(
 //          body1: TextStyle(color: Colors.black54),
